34682062c028881068c6e4bd6c9eb06bb4d63af2
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-09-10 22:10:14 -0700

    Add color on mouseenter

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -81,10 +81,15 @@ $(function () {
 
 // const Board = require('./board');
 
+window.randomColorString = function() {
+  return '#' + Math.random().toString(16).substr(-6);
+}
+
 class View {
   constructor($el) {
     this.$el = $el;
     this.setupBoard();
+    this.level0();
   }
 
   setupBoard() {
@@ -95,7 +100,7 @@ class View {
       this.$el.append($button);
     }
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 30; i++) {
       this.addRow();
     }
 
@@ -104,12 +109,20 @@ class View {
   addRow() {
     const rowIdx = this.$el.find(".row").length;
     const $row = $("<ul>").addClass("row").addClass("group");
-    for (let colIdx = 0; colIdx < 20; colIdx++) {
+    for (let colIdx = 0; colIdx < 30; colIdx++) {
       const $square = $("<li>").addClass("square").attr("data-pos", [rowIdx, colIdx]);
 
       $row.append($square);
     }
     this.$el.append($row);
+  }
+
+  level0() {
+    console.log("something");
+    $('.square').on("mouseover", e => {
+      const $sq = $(e.currentTarget);
+      $sq.css("background-color",  randomColorString());
+    });
   }
 
   render() {
@@ -126,6 +139,7 @@ class View {
   }
 
 }
+
 
 View.KEYS = {
 
