cd04e7977b691881a9199c4e924bd9a3643c4d4e
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-09-17 23:21:13 -0700

    Add factory method, render board

--- docs/js/board.js
+++ docs/js/board.js
@@ -4,20 +4,36 @@ class Board {
   constructor(dim) {
     this.dim = dim;
     this.snake = new Snake(this);
-    console.log(this.snake);
   }
 
   static blankGrid(dim) {
+    const grid = [];
 
+    for (let i = 0; i < dim; i++) {
+      const row = [];
+      for (let j = 0; j < dim; j++) {
+        row.push(Board.BLANK_SYMBOL);
+      }
+      grid.push(row);
+    }
+
+    return grid;
   }
 
   render() {
+    const grid = Board.blankGrid(this.dim);
+
+    this.snake.segments.forEach( segment => {
+      grid[segment.i][segment.j] = Snake.SYMBOL;
+    });
 
   }
 
   validPosition(coord) {
-
+    return (coord.i >= 0) && (coord.i < this.dim) && (coord.j >= 0) && (coord.j < this.dim);
   }
 }
+
+Board.BLANK_SYMBOL = ".";
 
 module.exports = Board;
